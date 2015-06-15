034653caf225d019aab7129b1d8c1cc6f92eb529
sungtaek <leesungtaek@gmail.com>
2015-06-15 16:20:24 +0000

    ...

--- mongo/views.py
+++ mongo/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import logging
 from pymongo import MongoClient 
 import json
 
 # Create your views here.
+logger = logging.getLogger(__name__)
+
 def doc(request, col_id, doc_id):
     client = MongoClient('localhost', 27017)
     db = client['kkoma']
     col = db[col_id]
 
     if request.method == "GET":
+        logger.debug("get doc %s:%s" % (col_id, doc_id))
         doc = col.find_one({"_id": doc_id})
         return HttpResponse(doc)
     elif request.method == "POST":
+        logger.debug("post doc %s:%s" % (col_id, doc_id))
         doc = json.loads(request.POST['data'])
         col.insert(doc)
         result = dict()
@@ -20,4 +25,5 @@ def doc(request, col_id, doc_id):
         result['message'] = 'insert ' + doc_id + ' success'
         return HttpResponse(json.dumps(result), content_type='application/json')
     else :
+        logger.warn("not support method %s" % (request.method))
         return HttpResponse('not support method')
