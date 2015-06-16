ab4c9793e66926c0eaf0caff447fbad0be7809ef
sungtaek <leesungtaek@gmail.com>
2015-06-16 17:04:01 +0000

    ...

--- mongo/views.py
+++ mongo/views.py
@@ -66,7 +66,6 @@ def query(request, col_id):
     result['docs'] = []
     count = 0
     for doc in col.find():
-        logger.debug('-> ' + doc)
         result['docs'].append(doc)
         count += 1
     result['count'] = count
